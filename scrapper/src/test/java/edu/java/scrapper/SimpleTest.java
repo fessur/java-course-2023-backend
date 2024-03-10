61bc173cba7ec62ace37e14bf1a350e053a1f3de
Michael Volodin <mihael.volodin@gmail.com>
2024-03-10 21:43:35 +0300

    Finished hw4

---
+++ scrapper/src/test/java/edu/java/scrapper/SimpleTest.java
@@ -0,0 +1,76 @@
+package edu.java.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SimpleTest extends IntegrationTest {
+    @Test
+    public void testTablesExist() throws Exception {
+        try (Connection conn = DriverManager.getConnection(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(),
+            POSTGRES.getPassword()
+        )) {
+            try (Statement statement = conn.createStatement()) {
+                try (ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM information_schema.tables WHERE table_name = 'chat';")) {
+                    resultSet.next();
+                    String tableName = resultSet.getString("table_name");
+                    assertThat(tableName).isEqualToIgnoringCase("chat");
+                }
+                try (ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM information_schema.tables WHERE table_name = 'link';")) {
+                    resultSet.next();
+                    String tableName = resultSet.getString("table_name");
+                    assertThat(tableName).isEqualToIgnoringCase("link");
+                }
+                try (ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM information_schema.tables WHERE table_name = 'chat_link';")) {
+                    resultSet.next();
+                    String tableName = resultSet.getString("table_name");
+                    assertThat(tableName).isEqualToIgnoringCase("chat_link");
+                }
+            }
+        }
+    }
+    @Test
+    public void testColumnsExist() throws Exception {
+        try (Connection conn = DriverManager.getConnection(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(),
+            POSTGRES.getPassword()
+        )) {
+            try (Statement statement = conn.createStatement()) {
+                try (ResultSet columns = statement.executeQuery(
+                    "SELECT column_name FROM information_schema.columns WHERE table_name = 'chat' AND column_name IN ('id', 'created_at');")) {
+                    List<String> foundColumns = new ArrayList<>();
+                    while (columns.next()) {
+                        foundColumns.add(columns.getString("column_name"));
+                    }
+                    assertThat(foundColumns).containsExactlyInAnyOrder("id", "created_at");
+                }
+                try (ResultSet columns = statement.executeQuery(
+                    "SELECT column_name FROM information_schema.columns WHERE table_name = 'link' AND column_name IN ('id', 'url', 'created_at', 'last_check_time');")) {
+                    List<String> foundColumns = new ArrayList<>();
+                    while (columns.next()) {
+                        foundColumns.add(columns.getString("column_name"));
+                    }
+                    assertThat(foundColumns).containsExactlyInAnyOrder("id", "url", "created_at", "last_check_time");
+                }
+                try (ResultSet columns = statement.executeQuery(
+                    "SELECT column_name FROM information_schema.columns WHERE table_name = 'chat_link' AND column_name IN ('chat_id', 'link_id');")) {
+                    List<String> foundColumns = new ArrayList<>();
+                    while (columns.next()) {
+                        foundColumns.add(columns.getString("column_name"));
+                    }
+                    assertThat(foundColumns).containsExactlyInAnyOrder("chat_id", "link_id");
+                }
+            }
+        }
+    }
+}
