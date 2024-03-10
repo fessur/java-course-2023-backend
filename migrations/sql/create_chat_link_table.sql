a9f6de90c2eb77b7dd7b9fc9f172b06aa5ea9253
Michael Volodin <mihael.volodin@gmail.com>
2024-03-10 21:43:35 +0300

    Finished hw4

---
+++ migrations/sql/create_chat_link_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS chat_link
+(
+    chat_id BIGINT NOT NULL,
+    link_id BIGINT NOT NULL,
+    PRIMARY KEY (chat_id, link_id),
+    FOREIGN KEY (chat_id) REFERENCES chat (id) ON DELETE CASCADE,
+    FOREIGN KEY (link_id) REFERENCES link (id) ON DELETE CASCADE
+);
--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,10 +1,21 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Testcontainers
 public abstract class IntegrationTest {
@@ -21,7 +32,17 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        try (Connection connection = DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword())) {
+            Database database =
+                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            ResourceAccessor changeLogDir = new DirectoryResourceAccessor(
+                new File(".").toPath().toAbsolutePath().getParent().getParent().resolve("migrations"));
+            Liquibase liquibase =
+                new Liquibase("master.xml", changeLogDir, database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot run migrations", e);
+        }
     }
 
     @DynamicPropertySource
